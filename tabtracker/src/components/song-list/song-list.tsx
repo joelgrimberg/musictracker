9d8cc5277a7fd5bb5c557378ed2dc78909b70e4c
Robbert van Markus <rvanmarkus@qxperts.io>
2023-02-06 18:13:57 +0100

    refactored styling song-page to align content

--- tabtracker/src/components/song-list/song-list.tsx
+++ tabtracker/src/components/song-list/song-list.tsx
@@ -1,54 +1,70 @@
-import type { FunctionComponent } from "react";
+import { FunctionComponent, useCallback } from "react";
 import { api } from "../../utils/api";
-import Image from 'next/image'
+import Image from "next/image";
 import { useRouter } from "next/router";
-import type { FormEventHandler} from "react";
+import type { FormEventHandler } from "react";
 
 export const SongList: FunctionComponent = () => {
-  const { data: songs } = api.song.getAllSongs.useQuery()
+  const { data: songs } = api.song.getAllSongs.useQuery();
   const { push } = useRouter();
-  const spotifyImageLoader = ({ src , width, quality = 75 }: {src: string, width: number | undefined, quality?: number}) => {
-    return `${src}?w=${width}&q=${quality || 75}`
-  }
+  const spotifyImageLoader = ({
+    src,
+    width,
+    quality = 75,
+  }: {
+    src: string;
+    width: number | undefined;
+    quality?: number;
+  }) => {
+    return `${src}?w=${width}&q=${quality || 75}`;
+  };
 
-  const onSubmit: FormEventHandler = (event) => {
-    event.preventDefault();
-  }
-
-  const { mutate, isLoading } = api.song.removeTrack.useMutation({onSuccess: () => push("/")});
-  const removeTrack = ( uuid: string ) => {
-    return () => {
-      mutate({uuid})
-    }
-  }
+  const { mutate, isLoading } = api.song.removeTrack.useMutation({
+    onSuccess: () => push("/"),
+  });
+  const removeTrack = useCallback(
+    (uuid: string) => {
+      mutate({ uuid });
+    },
+    [mutate]
+  );
 
   return (
-    <div className="grid grid-cols-2 gap-4 rounded-xl sm:grid-cols-4 md:gap-8 bg-white/10 p-4 text-white">
-
-      {songs && songs.length? '': 'quickly add your first song so I can show it here 🥳' }
-      {songs && songs.map(({ title, uuid, coverUrl }) => (
-
-        <div
-          key={uuid}
-          className="rounded-xl bg-white/10 p-4 text-white hover:bg-white/20 hover: cursor-pointer group ">
-          <div className="grid grid-cols-2 h-10 w-400">
-            <div className="w-300">
-              <h2>{title}</h2>
+    <div className="grid grid-cols-2 gap-4 rounded-xl bg-white/10 p-4 text-white sm:grid-cols-4 md:gap-8">
+      {songs && songs.length
+        ? ""
+        : "quickly add your first song so I can show it here 🥳"}
+      {songs &&
+        songs.map(({ title, uuid, coverUrl }) => (
+          <div
+            key={uuid}
+            className="hover: group cursor-pointer rounded-xl bg-white/10 p-4 text-white hover:bg-white/20 "
+          >
+            <div className="mb-2 grid grid-flow-col align-middle">
+              <h2 className="leading-6">{title}</h2>
+              <button
+                disabled={isLoading}
+                onClick={() => removeTrack(uuid)}
+                type="submit"
+                className="flex justify-end align-middle"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="hidden h-6 w-6 hover:bg-white/20 group-hover:inline-block"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
             </div>
 
-            <div className='flex justify-end'>
-              <form onSubmit={onSubmit} key={uuid} className="flex flex-col gap-4">
-                <input type="hidden" name="songId" value={uuid} />
-                <button disabled={isLoading} onClick={removeTrack(uuid)} type="submit">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hidden fill-slate-400 hover:fill-red-600 group-hover:inline-block">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                  </svg>
-                </button>
-              </form>
-            </div>
-          </div>
-
-          <div className="col-span-2">
             <Image
               loader={spotifyImageLoader}
               src={coverUrl}
@@ -57,9 +73,7 @@ export const SongList: FunctionComponent = () => {
               height={300}
             />
           </div>
-
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
