0eec351d8458e6850e5818b71231cb489d741f2d
Robbert van Markus <rvanmarkus@qxperts.io>
2023-02-06 22:33:17 +0100

    removing some anys and optimizing existing code

--- tabtracker/src/pages/create.tsx
+++ tabtracker/src/pages/create.tsx
@@ -1,31 +1,33 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import type { FormEventHandler} from "react";
-import { useState } from "react";
+import type { FormEventHandler } from "react";
+import { useState, useCallback } from "react";
 import { Layout } from "../components/layout/layout";
 import { api } from "../utils/api";
 
 const CreateSongPage: NextPage = () => {
   const { push } = useRouter();
   const [searchQuery, setSearchQuery] = useState("");
-  const { data: searchResults } = api.song.searchTrack.useQuery(
-    searchQuery,
-    { enabled: searchQuery !== "" }
+  const { data: searchResults } = api.song.searchTrack.useQuery(searchQuery, {
+    enabled: searchQuery !== "",
+  });
+  const { mutate: addTrack, isLoading } = api.song.addTrack.useMutation({
+    onSuccess: () => push("/"),
+  });
+
+  const onSubmit: FormEventHandler = useCallback(
+    (event) => {
+      event.preventDefault();
+      setSearchQuery(
+        (
+          (event.target as HTMLFormElement).elements.namedItem(
+            "search"
+          ) as HTMLInputElement
+        ).value
+      );
+    },
+    [setSearchQuery]
   );
-  const onSubmit: FormEventHandler = (event) => {
-    event.preventDefault();
-
-    // @typescript-eslint/no-explicit-any
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    setSearchQuery((event.target as any).search.value);
-  };
-
-  const { mutate, isLoading } = api.song.addTrack.useMutation({onSuccess: () => push("/")});
-  const addTrack = (data: { title: string; artist: string; coverUrl: string; spotifyId: string; }) => {
-    return () => { 
-      mutate(data)
-    }
-  }
 
   return (
     <Layout>
@@ -38,12 +40,24 @@ const CreateSongPage: NextPage = () => {
               <li key={item.id}>
                 {item.name} -{" "}
                 {item.artists.map((artist) => artist.name).join(" ")}
-                <button className="px-4 text-pink-300" disabled={isLoading} onClick={addTrack({
-                  title: item.name,
-                  artist: item.artists.map((artist) => artist.name).join(" "),
-                  coverUrl: item.album.images[0]?.url || 'https://picsum.photos/200/200',
-                  spotifyId: item.id,
-                })}>Add this song!</button>
+                <button
+                  className="px-4 text-pink-300"
+                  disabled={isLoading}
+                  onClick={() =>
+                    addTrack({
+                      title: item.name,
+                      artist: item.artists
+                        .map((artist) => artist.name)
+                        .join(" "),
+                      coverUrl:
+                        item.album.images[0]?.url ||
+                        "https://picsum.photos/200/200",
+                      spotifyId: item.id,
+                    })
+                  }
+                >
+                  Add this song!
+                </button>
               </li>
             ))}
         </ul>
