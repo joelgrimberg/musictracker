0ce1ed0776bebca2f48fb125e9486f585688979d
Robbert van Markus <rvanmarkus@qxperts.io>
2023-07-30 17:36:58 +0200

    setup of tanstack router / pages structure

--- musictracker/src/client/components/sidebar.tsx
+++ musictracker/src/client/components/sidebar.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 
 import { Playlist } from "../data/playlists"
+import { Link } from "@tanstack/router"
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
   playlists: Playlist[]
@@ -17,40 +18,44 @@ export function Sidebar({ className, playlists }: SidebarProps) {
             Discover
           </h2>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <circle cx="12" cy="12" r="10" />
-                <polygon points="10 8 16 12 10 16 10 8" />
-              </svg>
-              Listen Now
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="mr-2 h-4 w-4"
-              >
-                <rect width="7" height="7" x="3" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="3" rx="1" />
-                <rect width="7" height="7" x="14" y="14" rx="1" />
-                <rect width="7" height="7" x="3" y="14" rx="1" />
-              </svg>
-              Browse
-            </Button>
+            <Link to="/">
+              <Button variant="ghost" className="w-full justify-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4"
+                >
+                  <circle cx="12" cy="12" r="10" />
+                  <polygon points="10 8 16 12 10 16 10 8" />
+                </svg>
+                Listen Now
+              </Button>
+            </Link>
+            <Link to="/browse">
+              <Button variant="ghost" className="w-full justify-start">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="mr-2 h-4 w-4"
+                >
+                  <rect width="7" height="7" x="3" y="3" rx="1" />
+                  <rect width="7" height="7" x="14" y="3" rx="1" />
+                  <rect width="7" height="7" x="14" y="14" rx="1" />
+                  <rect width="7" height="7" x="3" y="14" rx="1" />
+                </svg>
+                Browse
+              </Button>
+            </Link>
             <Button variant="ghost" className="w-full justify-start">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
---
+++ musictracker/src/client/router.ts
@@ -0,0 +1,30 @@
+import { RootRoute, Route, Router } from "@tanstack/router"
+import { Index } from "./pages"
+import App from "./App"
+import { Browse } from "./pages/browse"
+
+const rootRoute = new RootRoute({
+    component: App,
+})
+
+const indexRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/',
+    component: Index,
+})
+const browseRoute = new Route({
+    getParentRoute: () => rootRoute,
+    path: '/browse',
+    component: Browse,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, browseRoute])
+
+export const router = new Router({ routeTree })
+
+// needed for maximum type safety
+declare module '@tanstack/router' {
+    interface Register {
+        router: typeof router
+    }
+}
