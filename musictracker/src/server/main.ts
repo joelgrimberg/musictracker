1509cf363a3ead8c90b381d51f1a7a32cf4d7e95
Robbert van Markus <rvanmarkus@qxperts.io>
2023-07-30 20:36:42 +0200

    most straightforward impl which satisfies contract

--- musictracker/src/server/main.ts
+++ musictracker/src/server/main.ts
@@ -1,11 +1,30 @@
+import { createExpressEndpoints, initServer } from "@ts-rest/express";
+import bodyParser from "body-parser";
 import express from "express";
+import { contract } from "../contract";
 import ViteExpress from "vite-express";
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
+const s = initServer();
+
+const router = s.router(contract, {
+  getPlaylist: async () => {
+    return { status: 200, body: { id: '1', title: 'a' } }
+  },
+  getPlaylists: async () => {
+    return {
+      status: 200, body: []
+    }
+  },
+  createPlaylist: async () => {
+    return { status: 201, body: { id: '1', title: 'created' } }
+  }
 });
+
+createExpressEndpoints(contract, router, app);
 
 ViteExpress.listen(app, 3000, () =>
   console.log("Server is listening on port 3000...")
