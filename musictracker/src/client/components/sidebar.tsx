2023179b9f78006e0edfd296f6d5db691b3dc1be
Robbert van Markus <rvanmarkus@qxperts.io>
2023-08-02 22:33:30 +0200

    use playlists query in sidebar

--- musictracker/src/client/components/sidebar.tsx
+++ musictracker/src/client/components/sidebar.tsx
@@ -2,14 +2,14 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ScrollArea } from "@/components/ui/scroll-area"
 
-import { Playlist } from "../data/playlists"
 import { Link } from "@tanstack/router"
+import { client } from "@/client"
 
 interface SidebarProps extends React.HTMLAttributes<HTMLDivElement> {
-  playlists: Playlist[]
 }
 
-export function Sidebar({ className, playlists }: SidebarProps) {
+export function Sidebar({ className }: SidebarProps) {
+  const { isLoading, data } = client.getPlaylists.useQuery(['playlists'])
   return (
     <div className={cn("pb-12", className)}>
       <div className="space-y-4 py-4">
@@ -175,31 +175,33 @@ export function Sidebar({ className, playlists }: SidebarProps) {
           </h2>
           <ScrollArea className="h-[300px] px-1">
             <div className="space-y-1 p-2">
-              {playlists?.map((playlist, i) => (
-                <Button
-                  key={`${playlist}-${i}`}
-                  variant="ghost"
-                  className="w-full justify-start font-normal"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="mr-2 h-4 w-4"
+              {isLoading && "Loading playlists..."}
+              {data?.body?.length === 0 ? <p className="font-normal text-sm">No playlists yet...</p> :
+                data?.body?.map((playlist) => (
+                  <Button
+                    key={`playlist-${playlist?.id}`}
+                    variant="ghost"
+                    className="w-full justify-start font-normal"
                   >
-                    <path d="M21 15V6" />
-                    <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
-                    <path d="M12 12H3" />
-                    <path d="M16 6H3" />
-                    <path d="M12 18H3" />
-                  </svg>
-                  {playlist}
-                </Button>
-              ))}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="mr-2 h-4 w-4"
+                    >
+                      <path d="M21 15V6" />
+                      <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                      <path d="M12 12H3" />
+                      <path d="M16 6H3" />
+                      <path d="M12 18H3" />
+                    </svg>
+                    {playlist?.title}
+                  </Button>
+                ))}
             </div>
           </ScrollArea>
         </div>
