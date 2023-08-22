2d463431217b9ee7efc648e3fb4edfd7558160d8
Robbert van Markus <rvanmarkus@qxperts.io>
2023-08-22 22:22:01 +0200

    format date added in tracklist table

--- musictracker/src/client/components/tracklist/columns.tsx
+++ musictracker/src/client/components/tracklist/columns.tsx
@@ -1,6 +1,10 @@
 import { ColumnDef } from "@tanstack/react-table";
 import { TrackSchema } from "../../../contract";
 import { z } from "zod";
+const dateFormatter = new Intl.DateTimeFormat("en", {
+  dateStyle: "medium",
+  timeStyle: "short",
+});
 
 export const columns: ColumnDef<z.infer<typeof TrackSchema>>[] = [
   {
@@ -14,5 +18,9 @@ export const columns: ColumnDef<z.infer<typeof TrackSchema>>[] = [
   {
     accessorKey: "createdAt",
     header: "Date added",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("createdAt"));
+      return <span>{dateFormatter.format(date)}</span>;
+    },
   },
 ];
