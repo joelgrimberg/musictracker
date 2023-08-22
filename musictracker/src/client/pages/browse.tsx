7e4724a1b7cdf53dbd0114bcfc6377625206b3d2
Robbert van Markus <rvanmarkus@qxperts.io>
2023-08-22 22:22:46 +0200

    loading spinner browse apge

--- musictracker/src/client/pages/browse.tsx
+++ musictracker/src/client/pages/browse.tsx
@@ -1,15 +1,15 @@
 import { client } from "@/client";
 import { columns } from "@/components/tracklist/columns";
 import { Tracklist } from "@/components/tracklist/tracklist";
+import { Spinner } from "@/components/ui/spinner";
 
 export function Browse() {
   const { data, isLoading } = client.getAllMusicTracks.useQuery(["tracks"]);
 
-  if (isLoading) return "Loading...";
-
   return (
     <div className="space-y-6">
       <Tracklist columns={columns} data={data?.body ?? []} />
+      {isLoading && <Spinner />}
     </div>
   );
 }
