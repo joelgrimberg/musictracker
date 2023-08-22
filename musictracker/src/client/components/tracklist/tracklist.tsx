9282c633082a86b1dd992176019a861cb286ce54
Robbert van Markus <rvanmarkus@qxperts.io>
2023-08-22 22:22:32 +0200

    align cell content tacklist

--- musictracker/src/client/components/tracklist/tracklist.tsx
+++ musictracker/src/client/components/tracklist/tracklist.tsx
@@ -58,7 +58,7 @@ export function Tracklist<TData, TValue>({
                 data-state={row.getIsSelected() && "selected"}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell key={cell.id} align="left">
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
